f2ba4ea76cbd36cb9fe34546cc9e7f8d553b8997
Mason <m_nia@outlook.com>
2025-03-11 14:00:27 +1100

    added instructions

--- calmicro.js
+++ calmicro.js
@@ -1,7 +1,10 @@
+// Import required modules
 const express = require("express");
+// Create an Express application
 const app = express();
 const fs = require("fs");
 const winston = require("winston");
+// Configure Winston Logger
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
@@ -20,6 +23,7 @@ const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
+// Enable logging in the console when not in production
 if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
@@ -27,17 +31,21 @@ if (process.env.NODE_ENV !== "production") {
     })
   );
 }
+//Function to add two numbers
 const add = (n1, n2) => {
   return n1 + n2;
 };
+// API Route for Addition
 app.get("/add", (req, res) => {
   try {
+    // Parse query parameters as floating point numbers
     const n1 = parseFloat(req.query.n1);
     const n2 = parseFloat(req.query.n2);
     if (isNaN(n1)) {
       logger.error("n1 is incorrectly defined");
       throw new Error("n1 incorrectly defined");
     }
+    // Validate the inputs
     if (isNaN(n2)) {
       logger.error("n2 is incorrectly defined");
       throw new Error("n2 incorrectly defined");
@@ -47,15 +55,22 @@ app.get("/add", (req, res) => {
       console.log();
       throw new Error("Parsing Error");
     }
+    // Log the received parameters
     logger.info("Parameters " + n1 + " and " + n2 + " received for addition");
+    // Perform addition
     const result = add(n1, n2);
+    // Perform addition
     res.status(200).json({ statuscocde: 200, data: result });
   } catch (error) {
+    // Log errors
     console.error(error);
+    // Return error response
     res.status(500).json({ statuscocde: 500, msg: error.toString() });
   }
 });
+// Define the port number
 const port = 3040;
+// Start the server
 app.listen(port, () => {
   console.log("hello i'm listening to port " + port);
 });
